38a01fcce0513dfa568fddd5956aa4f6831cddc5
Sugiwamaa <106767936+Sugiwamaa@users.noreply.github.com>
2022-10-27 17:35:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+ var signo = prompt('Digite seu signo: ')
+
+ switch (signo) {
+case "Touro":
+    
+    console.log("21 de Abril até 21 de Maio!")
+    
+        break;
+ 
+case "Aries":
+    
+    console.log("21 de Março a 20 de Abril")
+    
+        break;
+
+case "Gêmeos":
+    
+    console.log("21 de Maio até 20 de junho!")
+    
+        break;
+    
+
+case "Câncer":
+    
+    console.log("21 de Junho até 22 de Julho!")
+    
+        break;
+
+        case "Leão":
+    
+            console.log("23 de Julho até 22 de Agosto!")
+            
+                break;
+
+case "Virgem":
+    
+    console.log("23 de Agosto até 22 de Setembro!")
+    
+        break;
+        
+                
+default:
+    
+        console.log("ESte signo não tem registro...")    
+    
+        break;
+ }
